e51858f02d5bad65dd775eef3a14194ea41d41af
”名前” <>
2019-03-23 18:58:15 +0900

    first commit

---
+++ resources/views/admin/home/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+@section('title', 'Session       login search')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h1 class="top">Session</h1>
+        <p class="top">-サーフィンを始めよう-</p>
+        <h2>サーフィンを始める前に<br>ルール＆マナー</h2>
+        <p class="how"><img src="../images/22c90bc4b326222b658f6327d0cf694e_s.png"></p>
+        <h3>サーフショップ＆サーフィンスクール</h3>
+        <p class="search"><img src="../images/78dbc91aa84f0ad0ec639e0f5ddab39d_s.png"></p>
+        <h4 class="nami">波情報</h4>
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/admin/howto/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('title', 'Session       login search')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h1>サーフィンを始める前に</h1>
+      </div>
+    </div>
+  </div>
+@endsection
